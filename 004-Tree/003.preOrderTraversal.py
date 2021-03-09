8fc059a219b596c03d68c12e2bb910d99e8e13ae
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-03-09 14:36:38 +0530

    Changed directory structure

---
+++ 004-Tree/003.preOrderTraversal.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def preOrder(root):
+    if root == None:
+        return
+    print(root.val,end=' ')
+    preOrder(root.left)
+    preOrder(root.right)
+
+'''
+        1
+       / \
+      2  3
+     /    \
+    5     9
+         /
+        7
+'''
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(5)
+root.right.right = Node(9)
+root.right.right.left = Node(7)
+
+
+preOrder(root)
+print()
