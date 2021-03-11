01d36c81293aef4a239d0b124af64eac2629e606
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-03-11 11:10:16 +0530

    Print adjacency list

--- 005-Graphs/003.print-adjacency-list.py
+++ 005-Graphs/003.print-adjacency-list.py
@@ -1,3 +1,9 @@
 '''
 Problem Link: https://practice.geeksforgeeks.org/problems/print-adjacency-list-1587115620/1
 '''
+class Solution:
+    def printGraph(self, V, adj):
+        ans = []
+        for i in range(0,V):
+            ans.append([i] + adj[i])
+        return ans
