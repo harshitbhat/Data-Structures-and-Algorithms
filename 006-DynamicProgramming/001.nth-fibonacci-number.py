7928c5607a1358bf70cfad47cb9c9d5a96575458
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-03-12 02:01:34 +0530

    Nth Fibonacci Number

---
+++ 006-DynamicProgramming/001.nth-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nthFibonacci (ob, n):
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        mod = 1000000007
+        for i in range(2,n+1):
+            dp[i] = (dp[i-1] % mod + dp[i-2] % mod) % mod
+        
+        return dp[n]
