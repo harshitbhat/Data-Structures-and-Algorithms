b6ba386e62b7f1cb892aaa7fd3613c6d66f905f7
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-15 17:00:48 +0530

    NOT operator

---
+++ 001-BitwiseOperators/006.NOToperator.py
@@ -0,0 +1,21 @@
+'''
+NOT Operator(~)
+There are signed integers in Python
+~ -6  =  5
+~ -5  =  4
+~ -4  =  3
+~ -3  =  2
+~ -2  =  1
+~ -1  =  0
+~ 0  =  -1
+~ 1  =  -2
+~ 2  =  -3
+~ 3  =  -4
+~ 4  =  -5
+~ 5  =  -6
+'''
+
+for i in range(-6,6):
+    print('~',i,' = ',~i)
+
+
