d59adeeb1a5db436a9d4ac10f2d2782575e9083e
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-15 16:51:45 +0530

    Left Shift Operator

---
+++ 001-BitwiseOperators/004.leftShift.py
@@ -0,0 +1,12 @@
+'''
+Left Shift Operator ( << ): It shifts the bit right by the amount specified and adds 0 in same amount to the RHS of the number, thus it multiplies the number.
+
+i.e x << y, this implies that resultant is x * (2^y)
+'''
+
+x = 3
+
+print(x << 1) # 6
+print(x << 2) # 12
+print(x << 3) # 24
+print(x << 4) # 48
