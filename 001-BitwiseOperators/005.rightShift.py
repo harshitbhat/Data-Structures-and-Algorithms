bf4026ed4774ae92752b564e81610a6bad3e50b0
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-15 16:55:10 +0530

    Right Shift Operator

---
+++ 001-BitwiseOperators/005.rightShift.py
@@ -0,0 +1,12 @@
+'''
+Right Shift Operator ( >> ): It shifts the bit right by the amount specified and adds 0 in same amount to the left side of the number, thus it divides the number.
+
+i.e x << y, this implies that resultant is floor of  x / (2^y)
+'''
+
+x = 55
+
+print(x >> 1) # 27
+print(x >> 2) # 13
+print(x >> 3) # 6
+print(x >> 4) # 3
