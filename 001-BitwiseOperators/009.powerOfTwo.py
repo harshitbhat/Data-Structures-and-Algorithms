48644bd1f5f95b2fb1bc8e15c2651fbebaa18911
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-15 18:05:07 +0530

    Check if a number is power of 2

---
+++ 001-BitwiseOperators/009.powerOfTwo.py
@@ -0,0 +1,22 @@
+# Naive Method
+def powerOfTwoN(n):
+    if n == 0:
+        return False
+    
+    while n != 1:
+        if n % 2 != 0:
+            return False
+        n = n // 2
+    return True
+
+print(powerOfTwoN(514))
+
+
+# Efficient, The number must have only one set bit
+
+def powerOfTwo(n):
+    if n == 0:
+        return False
+    return (n & (n-1)) == 0
+
+print(powerOfTwo(40))
