fe558fe1736a559751383dad4493ee5ff8e3855d
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-15 16:47:54 +0530

    Bitwise XOR

---
+++ 001-BitwiseOperators/003.bitwiseXOR.py
@@ -0,0 +1,13 @@
+'''
+XOR(^) will give a set bit if both bits are different and will give 0 if both are same.
+The symbol and working is same as that in C++
+
+3 = 0011
+6 = 0110
+
+(3 ^ 6) = (0101) = 5
+'''
+x = 3
+y = 6
+
+print(x ^ y) # It will give 5 as output
