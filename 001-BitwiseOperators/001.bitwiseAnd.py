6f71b9a45d2d1ffcab86af15edf1c6524d844410
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-15 16:43:39 +0530

    Bitwise AND

---
+++ 001-BitwiseOperators/001.bitwiseAnd.py
@@ -0,0 +1,14 @@
+'''
+AND(&) will give a set bit if both the bits are set and give 0 otherwise
+The symbol and working is same as that in C++
+
+3 = 0011
+6 = 0110
+
+(3 & 6) = (0010) = 2
+'''
+
+x = 3
+y = 6
+
+print(x & y) #gives 2 as output
