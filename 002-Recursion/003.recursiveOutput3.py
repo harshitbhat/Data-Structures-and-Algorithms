51fcdeb376674b5fd8634bf02984dc377ce70e6f
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-01 06:01:34 +0530

    003.recursiveOutput3.py

---
+++ 002-Recursion/003.recursiveOutput3.py
@@ -0,0 +1,17 @@
+# Function is generationg Power of 2 <= n => gives index
+def fun(n):
+    if n == 1:
+        return 0
+    
+    return 1 + fun(n//2)
+
+def main():
+    val = fun(64)
+    print(val)
+    '''
+    Output: 
+        4
+    '''
+
+if __name__ == '__main__':
+    main()
