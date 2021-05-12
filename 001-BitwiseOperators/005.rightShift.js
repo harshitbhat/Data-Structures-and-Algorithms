d905910e8d933d7fa8e5d3465125698e4b6396f9
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-12 18:27:52 +0530

    Right Shift Operator

---
+++ 001-BitwiseOperators/005.rightShift.js
@@ -0,0 +1,15 @@
+/*
+
+Right Shift Operator ( >> ): It shifts the bit right by the amount specified and adds 0 in same amount to the left side of the number, thus it divides the number.
+
+i.e x << y, this implies that resultant is floor of  x / (2^y)
+
+
+*/
+
+const x = 55;
+
+console.log(x >> 1);
+console.log(x >> 2);
+console.log(x >> 3);
+console.log(x >> 4);
