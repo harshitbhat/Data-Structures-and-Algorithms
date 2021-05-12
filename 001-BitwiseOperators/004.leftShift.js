a4b82b74f6f99617b8938d0b5b725dd13b6addbe
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-12 18:25:59 +0530

    Left Shift Operator

---
+++ 001-BitwiseOperators/004.leftShift.js
@@ -0,0 +1,13 @@
+/*
+
+Left Shift Operator ( << ): It shifts the bit left by the amount specified and adds 0 in same amount to the right side of the number, thus it multiplies the number.
+
+i.e x << y, this implies that resultant is x * (2^y)
+
+*/
+const x = 3;
+
+console.log(x << 1);
+console.log(x << 2);
+console.log(x << 3);
+console.log(x << 4);
