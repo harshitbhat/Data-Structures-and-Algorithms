02507415dadd45d10a9370b07638c4625ee7a7bd
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-12 18:22:51 +0530

    Bitwise OR

---
+++ 001-BitwiseOperators/002.bitwiseOR.js
@@ -0,0 +1,13 @@
+/*
+
+OR(|) will give a set bit if either of the bits are set and give 0 when both are zero.
+The symbol and working is same as that in C++
+
+3 = 0011
+6 = 0110
+
+(3 | 6) = (0111) = 7
+
+*/
+
+console.log(3 | 6);
