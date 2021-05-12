2edd95ff6a8fd63f05be19a84df45e520bc9757b
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-12 18:14:56 +0530

    Bitwise AND

---
+++ 001-BitwiseOperators/001.bitwiseAND.js
@@ -0,0 +1,13 @@
+/*
+
+AND(&) will give a set bit if both the bits are set and give 0 otherwise
+The symbol and working is same as that in C++
+
+3 = 0011
+6 = 0110
+
+(3 & 6) = (0010) = 2
+
+*/
+
+console.log(3 & 6);
