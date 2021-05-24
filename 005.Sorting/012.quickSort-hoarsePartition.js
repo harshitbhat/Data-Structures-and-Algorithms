a72a6ef7b60da02a6d9a00d3edb59ea78906c992
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-24 08:44:21 +0530

    QuickSort using Hoare Partition

--- 005.Sorting/012.quickSort-hoarsePartition.js
+++ 005.Sorting/012.quickSort-hoarsePartition.js
@@ -9,16 +9,19 @@ function partition(arr, low, high) {
     do {
       j--;
     } while (arr[j] > pivot);
-    if (i > j) return;
+    if (i >= j) return j;
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-  return i;
 }
 
-function quicksort(arr, low, high) {
+function quickSort(arr, low, high) {
   if (low < high) {
     const p = partition(arr, low, high);
-    quickSort(arr, low, p - 1);
+    quickSort(arr, low, p);
     quickSort(arr, p + 1, high);
   }
 }
+
+const arr = [8, 4, 7, 9, 20, 3];
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);
