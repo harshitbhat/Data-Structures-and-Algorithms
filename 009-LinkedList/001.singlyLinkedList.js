477d22840b22e07bb2d9b290180de1475f4d6121
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-28 22:24:08 +0530

    Insert at given position in Singly Linked List

--- 009-LinkedList/001.singlyLinkedList.js
+++ 009-LinkedList/001.singlyLinkedList.js
@@ -60,6 +60,27 @@ class LinkedList {
     }
     temp.next = null;
   }
+  // insert at a position
+  insert(val, pos) {
+    const newNode = new Node(val);
+    if (pos === 1) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+    let temp = this.head;
+    let ct = 1;
+    while (temp) {
+      if (ct === pos - 1) {
+        newNode.next = temp.next;
+        temp.next = newNode;
+        return;
+      }
+      temp = temp.next;
+      ct++;
+    }
+    return;
+  }
 }
 
 const head1 = new Node(10);
@@ -72,8 +93,8 @@ const head2 = new Node(10);
 const head3 = null;
 
 const list = new LinkedList();
-list.head = head1;
+list.head = head3;
 list.traverse(list.head);
 console.log('+'.repeat(10));
-list.deleteLastNode();
+list.insert(99, 2);
 list.traverse(list.head);
