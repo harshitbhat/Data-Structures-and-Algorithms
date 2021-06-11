b534f9385322ce583478db465da39e79c045e0fe
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-06-11 18:05:52 +0530

    N-ary Tree Postorder Traversal

---
+++ LeetCode/Topicwise/Tree/006.n-ary-tree-postorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+
+var postorder = function (root) {
+  const ans = [];
+  const postOrderTraversal = (root) => {
+    if (!root) return;
+    root.children.forEach((ch) => postOrderTraversal(ch));
+    ans.push(root.val);
+  };
+  postOrderTraversal(root);
+  return ans;
+};
