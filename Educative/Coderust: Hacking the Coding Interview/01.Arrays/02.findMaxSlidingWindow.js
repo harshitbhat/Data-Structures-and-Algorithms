bd9c7530e9550e2b11651840d462966e86390208
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-13 16:31:25 +0530

    Find Maximum in Sliding Window

--- Educative/Coderust: Hacking the Coding Interview/01.Arrays/02.findMaxSlidingWindow.js
+++ Educative/Coderust: Hacking the Coding Interview/01.Arrays/02.findMaxSlidingWindow.js
@@ -15,3 +15,40 @@ let findMaxSlidingWindow = function (arr, window_size) {
   }
   return result;
 };
+
+// Using Dequeue
+
+let ffindMaxSlidingWindow = function (arr, windowSize) {
+  let result = [];
+
+  if (arr.length === 0 || windowSize > arr.length) return result;
+
+  let window = [];
+
+  // find max for first window
+  for (let i = 0; i < windowSize; i++) {
+    while (window.length > 0 && arr[i] >= arr[window[window.length - 1]]) {
+      window.pop();
+    }
+    window.push(i);
+  }
+
+  // Put the first result
+  result.push(arr[window[0]]);
+
+  for (i = windowSize; i < arr.length; i++) {
+    // remove all elements that are smaller than current element from last
+    while (window.length > 0 && arr[i] >= arr[window[window.length - 1]]) {
+      window.pop();
+    }
+    // If current front is not in current window, remove it from window
+    if (window.length > 0 && window[0] <= i - windowSize) {
+      window.shift();
+    }
+    // Push the current index and put first element of window to resutl
+    window.push(i);
+    result.push(arr[window[0]]);
+  }
+
+  return result;
+};
