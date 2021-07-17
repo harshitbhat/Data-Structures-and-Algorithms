f2fb069b76d96b8d234a6370665042c01508d435
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-17 10:26:17 +0530

    Insert Implementation (Iterative)

--- Educative/Data Structures for Coding Interviews in JavaScript/05.Trees/00.BinarySearchTree.js
+++ Educative/Data Structures for Coding Interviews in JavaScript/05.Trees/00.BinarySearchTree.js
@@ -8,13 +8,45 @@ class Node {
 
 class BinarySearchTree {
   constructor(rootVal) {
-    this.root = new Node(rootVal);
+    this.root = rootVal ? new Node(rootVal) : null;
   }
 }
+
+BinarySearchTree.prototype.insertIt = function (newVal) {
+  if (this.root === null) {
+    this.root = new Node(newVal);
+    return;
+  }
+
+  let currentNode = this.root;
+  let parent;
+
+  while (currentNode) {
+    parent = currentNode;
+    if (newVal < currentNode.val) {
+      currentNode = currentNode.leftChild;
+    } else {
+      currentNode = currentNode.rightChild;
+    }
+  }
+
+  if (newVal < parent.val) {
+    parent.leftChild = new Node(newVal);
+  } else {
+    parent.rightChild = new Node(newVal);
+  }
+};
 
 /* -------------------------------------------------------------------------- */
 /*                                    Test                                    */
 /* -------------------------------------------------------------------------- */
+// 6,4,9,2,5,8,12,10,14
+const bst = new BinarySearchTree();
+bst.insertIt(6);
+bst.insertIt(4);
+bst.insertIt(9);
+bst.insertIt(2);
+bst.insertIt(5);
+bst.insertIt(8);
 
-const bst = new BinarySearchTree(9);
 console.log(bst);
