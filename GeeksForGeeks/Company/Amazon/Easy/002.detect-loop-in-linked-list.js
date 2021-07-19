06f7b53b39f6fbe57add6eb61b495d5c9664b3b0
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-19 21:29:26 +0530

    Detect Loop in linked list

--- GeeksForGeeks/Company/Amazon/Easy/002.detect-loop-in-linked-list.js
+++ GeeksForGeeks/Company/Amazon/Easy/002.detect-loop-in-linked-list.js
@@ -1,3 +1,5 @@
+// Question Link: https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1/
+
 class Solution {
   //Function to check if the linked list has a loop.
   detectLoop(head) {
