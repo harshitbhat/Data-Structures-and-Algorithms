106cff7857b97de48d9627cd4b905453fc7f32d0
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-19 14:58:48 +0530

    Check if Arrays are Disjointed

---
+++ Educative/Data Structures for Coding Interviews in JavaScript/07.Heap/02.isDisjoint.js
@@ -0,0 +1,12 @@
+function isDisjoint(list1, list2) {
+  const myHash = {};
+  for (const el of list1) {
+    myHash[el] = true;
+  }
+  for (const el of list2) {
+    if (myHash[el]) {
+      return false;
+    }
+  }
+  return true;
+}
