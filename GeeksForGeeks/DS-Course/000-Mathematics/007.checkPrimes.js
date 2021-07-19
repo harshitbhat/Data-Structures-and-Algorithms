aa2624c1dc5b45456657a8adb4027aa73c0a12dd
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-20 01:20:27 +0530

    Rectified error in Code

--- GeeksForGeeks/DS-Course/000-Mathematics/007.checkPrimes.js
+++ GeeksForGeeks/DS-Course/000-Mathematics/007.checkPrimes.js
@@ -41,7 +41,7 @@ function checkPrimes(n) {
     return false;
   }
   let i = 5;
-  while (i * i < n) {
+  while (i * i <= n) {
     if (n % i == 0 || n % (i + 2) == 0) {
       return false;
     }
