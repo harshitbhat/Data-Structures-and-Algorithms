d3d4367ee495b9678493d0eab0ef34ea241547ee
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-20 16:31:49 +0530

    Equal Subset Sum Partition

--- Educative/Dynamic Programming Patterns/Pattern 1: 0-1 Knapsack/03.equalSumPartition.js
+++ Educative/Dynamic Programming Patterns/Pattern 1: 0-1 Knapsack/03.equalSumPartition.js
@@ -37,7 +37,30 @@ function solveTD(dp, num, sum, currentIndex) {
 }
 
 // Bottom Up Dynamic Programming
-const solveBU = function (nums, sum) {};
+const solveBU = function (nums, sum) {
+  const dp = new Array(nums.length)
+    .fill(false)
+    .map(() => Array(sum + 1).fill(false));
+
+  for (let i = 0; i < nums.length; i++) {
+    dp[i][0] = true;
+  }
+
+  for (let s = 1; s <= sum; s++) {
+    if (nums[0] === s) {
+      dp[0][s] = true;
+    }
+  }
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let s = 1; s <= sum; s++) {
+      dp[i][s] =
+        nums[i] <= s ? dp[i - 1][s] || dp[i - 1][s - nums[i]] : dp[i - 1][s];
+    }
+  }
+
+  return dp[nums.length - 1][sum];
+};
 
 let canPartition = function (num) {
   const sum = sumArray(num);
