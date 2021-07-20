34ce6136f05386456720531216db304e61b43f7b
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-20 14:32:52 +0530

    Escape characters

---
+++ Educative/JavaForProgrammers/01-BasicsofJavaProgramming/Ged.java
@@ -0,0 +1,5 @@
+public class Ged {
+    public static void main(String[] args) {
+        System.out.println("\"Good,\" said the boy, for he had no wish to tell the secret to his playmates, liking to know and do what they knew not and could not.");
+    }
+}
