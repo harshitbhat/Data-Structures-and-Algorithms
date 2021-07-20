d6e2fd034430145e54dde304b91f28c66daaaf00
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-20 17:06:55 +0530

    BFS of graph

---
+++ GeeksForGeeks/Company/Amazon/Easy/011.bfs-traversal-of-graph.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} V
+ * @param {number[][]} adj
+ * @returns {number[]}
+ */
+class Solution {
+  bfs(graph, visited) {
+    const ans = [];
+    const queue = [];
+
+    queue.push(0);
+    visited[0] = true;
+
+    while (queue.length > 0) {
+      const front = queue.shift();
+      ans.push(front);
+      for (const el of graph[front]) {
+        if (!visited[el]) {
+          queue.push(el);
+          visited[el] = true;
+        }
+      }
+    }
+
+    return ans;
+  }
+
+  bfsOfGraph(V, adj) {
+    const visited = new Array(V).fill(false);
+    return this.bfs(adj, visited);
+  }
+}
