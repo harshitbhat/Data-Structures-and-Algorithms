6e740bc256068787d885c47b477cc92c6b7a1660
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-20 13:02:57 +0530

    Beatles

---
+++ Educative/Java for Programmers/01-Basics of Java Programming/01.Beatles.java
@@ -0,0 +1,7 @@
+class Beatles {
+    public static void main(String []args) {
+        System.out.println("Hello, hello.");
+        System.out.println("I don't know why you say goodbye.");
+        System.out.println("I say hello.");
+    }
+}
