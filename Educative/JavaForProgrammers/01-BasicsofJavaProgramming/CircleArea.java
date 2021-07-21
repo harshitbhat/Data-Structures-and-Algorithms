fa3b3badacd30a9fa45929120ff219b2b11e3f3c
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-21 15:05:46 +0530

    CircleArea

---
+++ Educative/JavaForProgrammers/01-BasicsofJavaProgramming/CircleArea.java
@@ -0,0 +1,9 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        double PI = 3.14159;
+        int radius = 10;
+
+        double area = PI * radius * radius;
+        System.out.println(area);
+    }
+}
