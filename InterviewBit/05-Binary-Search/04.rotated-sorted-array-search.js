349e801c9c19e954e37d71104d3b5e14ea280f1e
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-07-27 16:18:08 +0530

    Rotated Sorted Array Search - FURTHER STUDY REQUIRED

---
+++ InterviewBit/05-Binary-Search/04.rotated-sorted-array-search.js
@@ -0,0 +1,9 @@
+// To study further.
+module.exports = {
+  //param A : array of integers
+  //param B : integer
+  //return an integer
+  search: function (A, B) {
+    return A.indexOf(B);
+  },
+};
