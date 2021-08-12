9c74c554dc9c979799a844a9a56668047465765b
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-08-12 15:55:10 +0530

    Added Recursive solution

--- GeeksForGeeks/DS-Course/016-Graph/03.depth-first-traversal-for-a-graph.js
+++ GeeksForGeeks/DS-Course/016-Graph/03.depth-first-traversal-for-a-graph.js
@@ -34,3 +34,27 @@ class Solution {
     return ans;
   }
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                  Recursive                                 */
+/* -------------------------------------------------------------------------- */
+
+const DFSRec = (graph, src, visited, ans) => {
+  visited[src] = true;
+  ans.push(src);
+  for (const node of graph[src]) {
+    if (!visited[node]) {
+      DFSRec(graph, node, visited, ans);
+    }
+  }
+};
+
+class Solution {
+  //Function to return a list containing the DFS traversal of the graph.
+  dfsOfGraph(V, adj) {
+    const visited = new Array(V).fill(false);
+    const ans = [];
+    DFSRec(adj, 0, visited, ans);
+    return ans;
+  }
+}
