06f45bebdbbb7bbbf3d70239ac009167175c36f1
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-08-15 10:09:25 +0530

    Stairs

---
+++ InterviewBit/13-Dynamic-Programming/02.stairs.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //param A : integer
+  //return an integer
+  climbStairs: function (n) {
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+  },
+};
