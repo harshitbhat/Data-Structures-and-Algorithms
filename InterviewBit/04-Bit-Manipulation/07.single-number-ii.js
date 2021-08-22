d0cbfc90dea8faff68af85a8657f101b6ca565b4
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-08-22 11:13:50 +0530

    Single Number II

--- InterviewBit/04-Bit-Manipulation/07.single-number-ii.js
+++ InterviewBit/04-Bit-Manipulation/07.single-number-ii.js
@@ -17,3 +17,29 @@ module.exports = {
     }
   },
 };
+
+/* -------------------------------------------------------------------------- */
+/*                              Another Solutioň                             */
+/* -------------------------------------------------------------------------- */
+
+module.exports = {
+  //param A : array of integers
+  //return an integer
+  singleNumber: function (A) {
+    let ans = 0;
+    let x, sum;
+    for (let i = 0; i < 32; i++) {
+      sum = 0;
+      x = 1 << i;
+
+      for (const el of A) {
+        if (el & x) sum++;
+      }
+
+      if (sum % 3 !== 0) {
+        ans |= x;
+      }
+    }
+    return ans;
+  },
+};
